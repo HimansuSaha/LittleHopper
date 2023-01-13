f23af447534c83df0374bbfe704d3d44ba0bb62c
HimansuSaha <himansusaha@gmail.com>
2023-01-14 00:33:34 +0530

    second commit

--- littlehopper/src/app/app.module.ts
+++ littlehopper/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    ProfileComponent,
+    ProductComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' })
